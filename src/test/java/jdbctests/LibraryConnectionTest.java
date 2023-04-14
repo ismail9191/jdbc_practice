8542ba3968c7fcbaa2ee23078d70a2c24218076c
ismail <den.iz91@yahoo.com>
2023-04-14 03:10:29 +0300

    Initial commit

---
+++ src/test/java/jdbctests/LibraryConnectionTest.java
@@ -0,0 +1,42 @@
+package jdbctests;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+
+public class LibraryConnectionTest {
+
+
+   @Test
+    public void test() throws SQLException {
+
+       String dbUrl = "jdbc:mysql://100.26.213.91:3306/library1";
+       String dbUsername = "library1_client";
+       String dbPassword = "WVF4NdGXCKHeE6VQ";
+
+       Connection connection = DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+       Statement statement = connection.createStatement();
+       ResultSet resultSet = statement.executeQuery("SELECT * FROM books");
+
+       //once you set up connection default pointer looks for 0
+       //next() --> move pointer to first row
+       resultSet.next();
+
+       System.out.println(resultSet.getString(2));
+
+
+       //close connection
+       resultSet.close();
+       statement.close();
+       connection.close();
+
+
+
+
+
+
+
+   }
+
+
+}
